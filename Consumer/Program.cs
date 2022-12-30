edff4c0a47ca9c02def56a3f4d9e793ba572932e
Alan Huanca <ahuanca@peruapps.com.pe>
2022-12-30 12:33:26 -0500

    Agregar archivos de proyecto.

---
+++ Consumer/Program.cs
@@ -0,0 +1,53 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+
+namespace Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Please provide the configuration file path as a command line argument");
+            }
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddIniFile(@"D:\ApacheKafkaConcluent\confluentdatos.txt")
+                .Build();
+
+            configuration["group.id"] = "kafka-dotnet-getting-started";
+            configuration["auto.offset.reset"] = "earliest";
+
+            const string topic = "topic_clients";
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            using (var consumer = new ConsumerBuilder<string, string>(
+                configuration.AsEnumerable()).Build())
+            {
+                consumer.Subscribe(topic);
+                try
+                {
+                    while (true)
+                    {
+                        var cr = consumer.Consume(cts.Token);
+                        Console.WriteLine($"Consumed event from topic {topic} with key {cr.Message.Key,-10} and value {cr.Message.Value}");
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ctrl-C was pressed.
+                }
+                finally
+                {
+                    consumer.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,55 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+
+namespace Producer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Please provide the configuration file path as a command line argument");
+            }
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddIniFile(@"D:\ApacheKafkaConcluent\confluentdatos.txt")
+                .Build();
+
+            const string topic = "topic_clients";
+
+            string[] users = { "eabara", "jsmith", "sgarcia", "jbernard", "htanaka", "awalther" };
+            string[] items = { "book", "alarm clock", "t-shirts", "gift card", "batteries" };
+
+            using (var producer = new ProducerBuilder<string, string>(
+                configuration.AsEnumerable()).Build())
+            {
+                var numProduced = 0;
+                Random rnd = new Random();
+                const int numMessages = 10;
+                for (int i = 0; i < numMessages; ++i)
+                {
+                    var user = users[rnd.Next(users.Length)];
+                    var item = items[rnd.Next(items.Length)];
+
+                    producer.Produce(topic, new Message<string, string> { Key = user, Value = item },
+                        (deliveryReport) =>
+                        {
+                            if (deliveryReport.Error.Code != ErrorCode.NoError)
+                            {
+                                Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Produced event to topic {topic}: key = {user,-10} value = {item}");
+                                numProduced += 1;
+                            }
+                        });
+                }
+
+                producer.Flush(TimeSpan.FromSeconds(10));
+                Console.WriteLine($"{numProduced} messages were produced to topic {topic}");
+            }
+        }
+    }
+}
